bcb32091c984569ec16bf38ce558abbaf74a7bb5
sweta <swetayadav2060@gmail.com>
2024-11-14 19:40:44 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,25 @@ import org.w3c.dom.ls.LSOutput;
 import java .util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("shopping mart");
+        System.out.println("welcome to shopping mart");
         System.out.println("-----------------");
-        System.out.println("1. Admin");
-        System.out.println("2. Customer");
-        System.out.println("3. Exit");
-        System.out.println("-----------------");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the choice between 1,2,3 ");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1: {
-                System.out.println("Welcome to Admin page");
 
-                Product prd=new Product(1,"xyz","electronic product",2000,2,"elctronic");
-                System.out.println(prd.toString());
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter admin id:");
+        String ID=scanner.nextLine();
+        System.out.println("enter admin password:");
+        String Password=scanner.nextLine();
+        login admin=new login();
+        if(admin.aunthenticate(ID,Password)){
+            System.out.println("welcome to Admin page");
+            Product prd=new Product(1,"xyz","electronic product",2000,2,"elctronic");
+            System.out.println(prd.toString());
 
-                break;
-            }
-            case 2: {
-                System.out.println("Welcome to Customer page");
-                break;
-            }
-            case 3: {
-                System.out.println("Thank you! and Exiting from the Application");
-                break;
-            }
-            default: {
-                System.out.println("Invalid choice! Enter the choice between 1,2 and 3");
-            }
+        }
+        else{
+            System.out.println("welcome to user page");
+        }
+
 
         }
     }
-}
